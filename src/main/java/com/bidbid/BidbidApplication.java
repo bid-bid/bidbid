d4bfb1e45c3d224579d2cde6e00ed01592ba2cb4
Hyeonjun Park <phjppo0918@gmail.com>
2022-11-21 11:05:10 +0900

    feat: init Setting

---
+++ src/main/java/com/bidbid/BidbidApplication.java
@@ -0,0 +1,13 @@
+package com.bidbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidbidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidbidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bidbid/BidbidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidbidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
