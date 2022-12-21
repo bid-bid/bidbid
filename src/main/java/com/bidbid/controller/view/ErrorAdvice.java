5bdf6bc91376fddff2012797c826b0d23f697a6a
Hyeonjun Park <phjppo0918@gmail.com>
2022-12-21 16:03:48 +0900

    fix : 회원 중복 예외처리

--- src/main/java/com/bidbid/controller/view/ErrorAdvice.java
+++ src/main/java/com/bidbid/controller/view/ErrorAdvice.java
@@ -3,8 +3,16 @@ package com.bidbid.controller.view;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.persistence.EntityExistsException;
+
 @ControllerAdvice
 public class ErrorAdvice {
+
+    @ExceptionHandler(EntityExistsException.class)
+    public String entityExists(EntityExistsException e) {
+        return "member/signup-form";
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public String error(RuntimeException e) {
         return "error/page500";
