dde6013c7c69d31833733ab9c86ec3deb08e1fc1
Hyeonjun Park <phjppo0918@gmail.com>
2022-12-21 16:15:26 +0900

    fix : 비로그인시 예외처리 설정

--- src/main/java/com/bidbid/controller/view/ErrorAdvice.java
+++ src/main/java/com/bidbid/controller/view/ErrorAdvice.java
@@ -3,6 +3,7 @@ package com.bidbid.controller.view;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.el.PropertyNotFoundException;
 import javax.persistence.EntityExistsException;
 
 @ControllerAdvice
@@ -11,6 +12,11 @@ public class ErrorAdvice {
     @ExceptionHandler(EntityExistsException.class)
     public String entityExists(EntityExistsException e) {
         return "member/signup-form";
+    }
+
+    @ExceptionHandler(PropertyNotFoundException.class)
+    public String notLogin(PropertyNotFoundException e) {
+        return "auth/login-form";
     }
 
     @ExceptionHandler(RuntimeException.class)
